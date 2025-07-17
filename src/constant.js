6b5dbbd145c788f025acee29b933b0930b85d9d1
Abhay <abhay.work.02@gmail.com>
2025-07-17 18:21:31 +0530

    changed teh base url.

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:3000'
-// export const baseUrl = 'https://threeb-auto-experts-backend.onrender.com/'
+// export const baseUrl = 'http://localhost:3000'
+export const baseUrl = 'https://threeb-auto-experts-backend.onrender.com/'
